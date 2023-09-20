93bc5bf8f6ae1722be68d6fde9776659a6615aec
Phil Smith <phil@amillionmonkeys.co.uk>
2023-09-20 13:52:20 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./docs/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F49800',
+        black: "#1E1E1C"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
