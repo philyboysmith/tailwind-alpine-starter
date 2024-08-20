e2f5b48eba6f4fa162b95cca7dcd6e4f33e31106
Phil Smith <phil@amillionmonkeys.co.uk>
2024-08-20 10:12:41 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#F49800',
-        black: "#1E1E1C"
-      }
+        primary: '#6ea2ac',
+        ocean: '#013e51',
+        dark: '#4d5957',
+        green: '#89a674',
+        red: '#e27c75',
+        gold: '#bb8241'
+      },
+        fontFamily: {
+            'sans': ['"Source Sans Pro"', 'sans-serif'],
+            'serif': ['"Source Serif 4"', 'serif'],
+        },
     },
   },
   variants: {
